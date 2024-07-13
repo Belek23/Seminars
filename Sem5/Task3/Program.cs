97efa7a2b63acf088fe481eddd9b2b4bbd4cca60
Belek Abdy <beshik2013@yandex.ru>
2024-07-13 12:54:58 +0300

    add Sem5:Task3

---
+++ Sem5/Task3/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+// Пример
+// 2 3 4 3
+// 4 3 4 1 => 2 + 3 + 5 = 10
+// 2 9 5 4
+
+int SumOfMainDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+int[,] CreateRandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = random.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "  ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Input number of columns: ");
+int columns = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Input min of matrix elements: ");
+int min = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine("Input max of matrix elements: ");
+int max = Convert.ToInt32(System.Console.ReadLine());
+
+int[,] matrix = CreateRandomMatrix(rows, columns, min, max);
+PrintMatrix(matrix);
+System.Console.WriteLine(SumOfMainDiagonal(matrix));
